28ae47e204ccdcf26136b9de8810719faf4cae6f
lihongjie <hongjie@superads.cn>
2023-12-08 15:13:42 +0800

    feat: variables solutions

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -3,9 +3,9 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-const NUMBER = 3;
+// 定义常量需要指明变量类型
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
