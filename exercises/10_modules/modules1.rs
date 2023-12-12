268f67ad5d2e31837c9d84b33025fcf9b84236b0
lihongjie <hongjie@superads.cn>
2023-12-12 16:53:33 +0800

    feat: modules solutions

--- exercises/10_modules/modules1.rs
+++ exercises/10_modules/modules1.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
@@ -11,7 +10,8 @@ mod sausage_factory {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    // 同级目录下的模块可以直接引用, 但必须是pub的 (未声明的默认是私有的)
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
