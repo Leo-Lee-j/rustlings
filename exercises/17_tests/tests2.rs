5064c2e1a1c5b30547e3f270ba24fdd2b6b97869
lihongjie <hongjie@superads.cn>
2023-12-13 18:16:24 +0800

    feat: tests solutions

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -6,12 +6,12 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
+    /// 这个宏接受两个参数，如果这两个参数的值不相等，那么它会引发一个 panic。这个宏用于检查两个值是否相等。
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(2, 2);
     }
 }
--- exercises/17_tests/tests4.rs
+++ exercises/17_tests/tests4.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint tests4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Rectangle {
     width: i32,
@@ -24,25 +23,33 @@ impl Rectangle {
 
 #[cfg(test)]
 mod tests {
+    use std::panic;
     use super::*;
 
     #[test]
     fn correct_width_and_height() {
         // This test should check if the rectangle is the size that we pass into its constructor
         let rect = Rectangle::new(10, 20);
-        assert_eq!(???, 10); // check width
-        assert_eq!(???, 20); // check height
+        assert_eq!(rect.width, 10); // check width
+        assert_eq!(rect.height, 20); // check height
     }
 
+    /*
+    catch_unwind 函数接受一个闭包作为参数，并执行这个闭包。如果闭包中发生了 panic，
+    catch_unwind 会捕获这个 panic，并返回一个 Err 结果。如果闭包正常执行完毕，
+    catch_unwind 会返回一个 Ok 结果，其中包含了闭包的返回值。
+     */
     #[test]
     fn negative_width() {
         // This test should check if program panics when we try to create rectangle with negative width
-        let _rect = Rectangle::new(-10, 10);
+        let _rect = panic::catch_unwind(|| Rectangle::new(-10, 10));
+        assert!(_rect.is_err());
     }
 
     #[test]
     fn negative_height() {
         // This test should check if program panics when we try to create rectangle with negative height
-        let _rect = Rectangle::new(10, -10);
+        let _rect = panic::catch_unwind(|| Rectangle::new(10, -10));
+        assert!(_rect.is_err());
     }
 }
